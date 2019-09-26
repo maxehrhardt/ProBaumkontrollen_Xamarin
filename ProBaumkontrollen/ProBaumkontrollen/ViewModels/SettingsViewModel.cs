bf9eb2c2286b4bdd4d055b70ef7e4bfd63c9bb3b
Maximilian Ehrhardt <max.ehrhardt@hotmail.de>
2019-09-26 21:14:37 +0200

    Added a Settings page

--- ProBaumkontrollen/ProBaumkontrollen/ViewModels/SettingsViewModel.cs
+++ ProBaumkontrollen/ProBaumkontrollen/ViewModels/SettingsViewModel.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using Xamarin.Forms;
 using ProBaumkontrollen.ViewModels.Base;
 
 namespace ProBaumkontrollen.ViewModels
 {
     public class SettingsViewModel : ViewModelBase
     {
+        public ICommand BackCommand => new Command(async () => await BackAsync());
+
+        private async Task BackAsync()
+        {
+            await NavigationService.NavigateToAsync<MainViewModel>();
+        }
+
     }
 }
